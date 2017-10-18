0bb202abc5a1cdfab3f2d87ca443fc35a7314893
nowos <31601190+nowos@users.noreply.github.com>
2017-10-18 20:17:59 +0200

    Update my_app.js

--- my_app.js
+++ my_app.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
 
 var description;
-var licznik = 0;
-
-// po naciśnięciu przycisku Add:  $(document).ready(function(){
-
-var description;
 var licznik = 1;
 
 // po naciśnięciu przycisku Add:  
@@ -31,64 +26,6 @@ var licznik = 1;
         //nowa wartośc description
       description = $('#it_1').val();
       (this).text(description);
-
-    });
-    
-// usunięcie zadania
-    $('li').dblclick(function(){
-       //nowa wartośc description
-     $(this).hide();
-
-    });
-
-     
-});//end of main f-ction
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    $('#bt_add').click(function(){
-//zmienna description przyjmuje wartość pola input    
-     description = $('#it_1').val();
-//tworzenie zmiennej element 
-    element = $('#li'+licznik);     
-     
-     
-  //IF jeśli wybrany element już jest widoczny - to przejdź do następnego   
-    if(element.css("visibility","visible")){
-    licznik = licznik + 1;  
-    element = $('#li'+ licznik);     
-    $(element).css({
-    "visibility":"visible"
-    });
- //wprowadź tekst z pola wprowadzani tekstu  
-    $(element).text(description); 
-        
-    }; //end of IF
-    
- }); //end of add task
- 
- 
-//edycja zadania
-    $('li').click(function(){
-        //nowa wartośc description
-      description = $('#it_1').val();
-      $(this).text(description);
 
     });
     
